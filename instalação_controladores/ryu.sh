27fc496932e51fc87d8e94e4adbbeb7f41c01df5
RandelSouza <randelsouza88@gmail.com>
2019-03-21 08:03:28 -0300

    update ryu.sh

--- instalação_controladores/ryu.sh
+++ instalação_controladores/ryu.sh
@@ -7,12 +7,12 @@
 # All of the code is freely available under the Apache 2.0 license. Ryu is fully written in Python.
 # Quick Start
 # Installing Ryu is quite easy:
-sudo pip install ryu
+# sudo pip install ryu
 # If you prefer to install Ryu from the source code:
 git clone git://github.com/osrg/ryu.git
 cd ryu; pip install .
 # If you want to write your Ryu application, have a look at Writing ryu application document. After writing your application, just type:
-ryu-manager yourapp.py
+# ryu-manager yourapp.py
 # Optional Requirements
 # Some functions of ryu require extra packages:
 # OF-Config requires lxml and ncclient
@@ -20,12 +20,12 @@ ryu-manager yourapp.py
 # BGP speaker (SSH console) requires paramiko
 # Zebra protocol service (database) requires SQLAlchemy
 # If you want to use these functions, please install the requirements:
-pip install -r tools/optional-requires
+# pip install -r tools/optional-requires
 # Please refer to tools/optional-requires for details.
 # Prerequisites
 # If you got some error messages at the installation stage, please confirm dependencies for building the required Python packages.
 # On Ubuntu(16.04 LTS or later):
-apt install gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
+# apt install gcc python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
 # Support
 # sRyu Official site is http://osrg.github.io/ryu/.
 # If you have any questions, suggestions, and patches, the mailing list is available at ryu-devel ML. The ML archive at Gmane is also
