a4913cfa8e11b89554be1d2cd0fd7aa7ff665a38
RandelSouza <randelsouza88@gmail.com>
2019-03-21 00:42:33 -0300

    "update ryu.sh"

--- instalação_controladores/ryu.sh
+++ instalação_controladores/ryu.sh
@@ -5,3 +5,4 @@
 # applications. Ryu supports various protocols for managing network devices, such as OpenFlow, Netconf, OF-config, etc. About OpenFlow,
 #Ryu supports fully 1.0, 1.2, 1.3, 1.4, 1.5 and Nicira Extensions.
 # All of the code is freely available under the Apache 2.0 license. Ryu is fully written in Python.
+# Quick Start
