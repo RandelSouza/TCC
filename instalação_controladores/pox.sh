8cba3e7e3bfcfa7e28c22f6858210f563b7056b4
RandelSouza <randelsouza88@gmail.com>
2019-03-21 01:00:18 -0300

    "update pox.sh"

--- instalação_controladores/pox.sh
+++ instalação_controladores/pox.sh
@@ -1,3 +1,4 @@
 # POX is a networking software platform written in Python
 
 # POX started life as an OpenFlow controller, but can now also function
+# as an OpenFlow switch, and can be useful for writing networking software
