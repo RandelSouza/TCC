a90d4e61e33db4082bda6825a2cdef375d84e849
RandelSouza <randelsouza88@gmail.com>
2019-03-21 00:42:13 -0300

    "update ryu.sh"

--- instalação_controladores/ryu.sh
+++ instalação_controladores/ryu.sh
@@ -1,3 +1,4 @@
 # Getting Started
 # What's Ryu
 # Ryu is a component-based software defined networking framework.
+# Ryu provides software components with well defined API's that make it easy for developers to create new network management and control
