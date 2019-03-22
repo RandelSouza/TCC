f532e9453ef778355a83d1d75f4b3a92f50aa1d9
RandelSouza <randelsouza88@gmail.com>
2019-03-22 11:41:38 -0300

    "update opendaylight.sh"

--- instalação_controladores/opendaylight.sh
+++ instalação_controladores/opendaylight.sh
@@ -26,3 +26,4 @@
 
 # A microservices architecture, in which a “microservice” is a particular protocol or service that a user wants to enable within their installation of the OpenDaylight controller, for example:
 # A plugin that provides connectivity to devices via the OpenFlow or BGP protocols
+# An L2-Switch or a service such as Authentication, Authorization, and Accounting (AAA).
