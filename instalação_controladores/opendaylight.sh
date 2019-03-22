c91a6cee5cb8e365d7f66c02de2465a2b01cf03c
RandelSouza <randelsouza88@gmail.com>
2019-03-22 11:41:27 -0300

    "update opendaylight.sh"

--- instalação_controladores/opendaylight.sh
+++ instalação_controladores/opendaylight.sh
@@ -24,3 +24,4 @@
 # What’s different about OpenDaylight
 # Major distinctions of OpenDaylight’s SDN compared to traditional SDN options are the following:
 
+# A microservices architecture, in which a “microservice” is a particular protocol or service that a user wants to enable within their installation of the OpenDaylight controller, for example:
