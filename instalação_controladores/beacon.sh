6becc33c3e8ed4ff4203145335849065425a836a
RandelSouza <randelsouza88@gmail.com>
2019-03-22 10:54:36 -0300

    "update beacon.sh"

--- instalação_controladores/beacon.sh
+++ instalação_controladores/beacon.sh
@@ -331,3 +331,4 @@ git clone git://gitosis.stanford.edu/beacon.git
 # Window ? Preferences ? Java ? Compiler then under JDK Compliance, change Compiler compliance level to 1.6.
 # Import the OpenFlowJ project
 
+# File ? Import ? General ? Existing Projects into Workspace, Select ~/git/openflowj as the root directory, click ok, then select all the projects, ensure copy projects into workspace is not checked and click finish.
