68e2c3c1f9211cb7dc3a154365b349da1bbb3953
RandelSouza <randelsouza88@gmail.com>
2019-03-22 11:10:24 -0300

    "update opendaylight.sh"

--- instalação_controladores/opendaylight.sh
+++ instalação_controladores/opendaylight.sh
@@ -1,2 +1,3 @@
 
 # Our Mission
+# Communications providers and enterprises alike are eager to build or adapt their networks to be more flexible and responsive to their organizations’ and customers’ needs. At the same time, they are driving network automation to improve operational efficiency. OpenDaylight, the largest open source SDN controller, is helping lead this transition.
