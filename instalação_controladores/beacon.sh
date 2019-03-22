80d8ccae2bd97234a630c68f0c7f4456bd2a343e
RandelSouza <randelsouza88@gmail.com>
2019-03-22 10:53:52 -0300

    "update beacon.sh"

--- instalação_controladores/beacon.sh
+++ instalação_controladores/beacon.sh
@@ -323,3 +323,4 @@ git clone git://gitosis.stanford.edu/beacon.git
 # Java 6 JDK & JRE
 # Eclipse for RCP and RAP Developers v3.7.0
 # Setup
+# Create a new Eclipse workspace if you are not starting from a fresh Eclipse install
