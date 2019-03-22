6cc7e8d0466042446da0cc5ac438946327f9df86
RandelSouza <randelsouza88@gmail.com>
2019-03-22 11:30:13 -0300

    "update opendaylight.sh"

--- instalação_controladores/opendaylight.sh
+++ instalação_controladores/opendaylight.sh
@@ -7,3 +7,4 @@
 #Use Cases
 #The OpenDaylight platform (ODL) provides a flexible common platform underpinning a wide variety of applications and Use Cases. Read about some of the most common use cases here – and then let your imagination run free.
 
+# OpenDaylight + Open Networking
