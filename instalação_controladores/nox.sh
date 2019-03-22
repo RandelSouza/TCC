560ed1b4c2eeca1485a80778ae9775fca7b66a45
RandelSouza <randelsouza88@gmail.com>
2019-03-22 01:01:24 -0300

    "update nox.sh"

--- instalação_controladores/nox.sh
+++ instalação_controladores/nox.sh
@@ -12,3 +12,4 @@
 # This version of NOX is a developers' release.  It is intended to provide
 # a programmatic platform for controlling one or more OpenFlow switches.
 # NOX can be extended both in C++ and provides an abstracted interface to
+# OpenFlow.  This distribution contains a set of example applications and
