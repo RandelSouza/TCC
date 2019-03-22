741cdb0b513ab791453ac3e012a50a2224169c67
RandelSouza <randelsouza88@gmail.com>
2019-03-22 10:55:38 -0300

    "update beacon.sh"

--- instalação_controladores/beacon.sh
+++ instalação_controladores/beacon.sh
@@ -342,3 +342,4 @@ git clone git://gitosis.stanford.edu/beacon.git
 # After the target has been resolved, click Set as Target Platform in the top right corner of the main.target window. (Note if you click before it has been resolved, you will receive an error). At this point all compilation errors should be gone.
 # Import the Beacon code style settings
 
+# Click Window ? Preferences. Then in the left column click Java ? Code Style ? Formatter, then click the Import button, and select ~/git/beacon/beacon_style_settings.xml and hit ok, then ensure the Active profile is Beacon.
