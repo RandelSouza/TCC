ea0edde1da713dee55fc683a2f66d0b57b00f814
RandelSouza <randelsouza88@gmail.com>
2019-03-22 11:10:32 -0300

    "update opendaylight.sh"

--- instalação_controladores/opendaylight.sh
+++ instalação_controladores/opendaylight.sh
@@ -1,3 +1,4 @@
 
 # Our Mission
 # Communications providers and enterprises alike are eager to build or adapt their networks to be more flexible and responsive to their organizations’ and customers’ needs. At the same time, they are driving network automation to improve operational efficiency. OpenDaylight, the largest open source SDN controller, is helping lead this transition.
+# OpenDaylight (ODL) is a modular open platform for customizing and automating networks of any size and scale. The OpenDaylight Project arose out of the SDN movement, with a clear focus on network programmability. It was designed from the outset as a foundation for commercial solutions that address a variety of use cases in existing network environments.
