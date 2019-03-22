3b662d1c3b796946e6c750b4b35b8fa18343c8fa
RandelSouza <randelsouza88@gmail.com>
2019-03-22 11:42:36 -0300

    "update opendaylight.sh"

--- instalação_controladores/opendaylight.sh
+++ instalação_controladores/opendaylight.sh
@@ -36,3 +36,4 @@
 # What you’ll find in this guide
 # To set up your environment, you first install OpenDaylight followed by the Apache Karaf features that offer the functionality you require. The OpenDaylight Getting Started Guide covers feature descriptions, OpenDaylight installation procedures, and feature installation.
 
+# The Getting Started Guide also includes other helpful information, with the following organization:
