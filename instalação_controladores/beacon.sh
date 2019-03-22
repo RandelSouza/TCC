3b0b35a8db651967da69fcb48c1b6f3839b0c58b
RandelSouza <randelsouza88@gmail.com>
2019-03-22 10:51:28 -0300

    "update beacon.sh"

--- instalação_controladores/beacon.sh
+++ instalação_controladores/beacon.sh
@@ -296,3 +296,4 @@ git clone git://gitosis.stanford.edu/beacon.git
 # Check out the remaining Guides
 # What is Beacon?
 # What is Beacon?
+# Beacon is a fast, cross-platform, modular, Java-based OpenFlow controller that supports both event-based and threaded operation.
