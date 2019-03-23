312a469447dd3b45fb66f5190f6e619abf9b9e5d
RandelSouza <randelsouza88@gmail.com>
2019-03-23 00:00:40 -0300

    "update maestro.sh"

--- instalação_controladores/maestro.sh
+++ instalação_controladores/maestro.sh
@@ -4,3 +4,4 @@
 # Maestro is a platform for achieving automatic and programmatic network control
 # functions using these modularized applications. Although this project focuses on building
 # an OpenFlow controller using Maestro, Maestro is not only limited to OpenFlow networks.
+# The programming framework of Maestro provides interfaces for:
