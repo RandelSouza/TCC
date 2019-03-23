abdd751c3c2f55138a753c5fc638538c054f6959
RandelSouza <randelsouza88@gmail.com>
2019-03-23 00:00:20 -0300

    "update maestro.sh"

--- instalação_controladores/maestro.sh
+++ instalação_controladores/maestro.sh
@@ -1,2 +1,3 @@
 # Maestro is an “operating system” for orchestrating network control applications.
 # Maestro provides interfaces for implementing modular network control applications
+# to access and modify state of the network, and coordinate their interactions.
