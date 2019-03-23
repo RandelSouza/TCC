312ac2c7f4e4b201157e48f52ee2444582052250
RandelSouza <randelsouza88@gmail.com>
2019-03-23 00:02:00 -0300

    "update maestro.sh"

--- instalação_controladores/maestro.sh
+++ instalação_controladores/maestro.sh
@@ -19,3 +19,4 @@
 # Multi-threaded – Takes full advantage of multi-core processors.
 # Maestro currently provides the control components for realizing either a learning switch network, or a routed network using OpenFlow switches. Some components such as the command line console, etc, are still not full-fledged. We plan to further enrich and enhance the functionality of Maestro in future releases. Notice that Maestro is licensed under the GNU Lesser General Public License version 2.1. Google-project’s LGPL directs only to the version 3, so we make it explicit here that Maestro # # is licensed under LGPL v2.1.
 #----------------------------------------------------------------------------------------------------------------------------------------------#
+
