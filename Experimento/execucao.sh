87e139f97a48c3bf2f25768d15bb121d178fb6c2
RandelSouza <randelsouza88@gmail.com>
2019-05-05 14:55:29 -0300

    "update"

--- Experimento/execucao.sh
+++ Experimento/execucao.sh
@@ -2,64 +2,68 @@
 
 source ./mininet_wifi.sh
 # Primeiro fator é o controlador sdn, os níveis são NOX, Opendaylight, Ryu, Floodlight, POX, Maestro, Trema e Beacon.
+# O Segundo fator é o número de nós IoT
+#controllerSDN=(NOX Opendaylight Ryu Floodlight POX Maestro Trema Beacon)
 
-#controllerSDN=(NOX Opendaylight Ryu Floodlight POX Maestro Trema Beacon) 
-
-controllerSDN=(Ryu Floodlight POX) 
+controllerSDN=(Ryu Floodlight POX)
+quantityNodes=(1 2 3 5 9 14)
 
 for controller in "${controllerSDN[@]}"
 do
 	case $controller in
 		"NOX")
 			echo "sim " $controller;
-    	;;    
-  
-		"Opendaylight")		
+    	;;
+
+		"Opendaylight")
 			echo "sim " $controller;
     	;;
 
 		"Ryu")
-			#echo "sim " $controller;			
-    	;;    
-  
-		"Floodlight")								
-			echo "Entrando no controlador: " $controller;
-			echo
-			cd /home/floodlight;
-			echo "Inicializando o controlador: " $controller;
-			touch log_experimento_execucao$controller.txt			
-			java -jar target/floodlight.jar >> log_experimento_execucao$controller.txt &
+			#echo "sim " $controller;
+    	;;
 
-			sleep 10
-		
-			echo "Estartando o mininet wifi e criando uma topologia simples...."			
-			topology 127.0.0.1 6653 &		
-
-			while [ $(ps -ef | grep "jar target/floodlight.jar" | wc -l) -eq 2 ];
+		"Floodlight")
+			for quantity in "${quantityNodes[@]}"
 			do
-				# echo  $(ps -ef | grep "target/floodlight.jar" | wc -l)			
-				echo "Experimento do controlador $controller ainda está sendo realizado..."
-				sleep 2
+				echo "Entrando no controlador: $controller, Número de nós: $quantity";
+				echo
+				cd /home/floodlight;
+				echo "Inicializando o controlador: " $controller;
+				touch log_experimento_execucao$controller.txt
+				java -jar target/floodlight.jar >> log_experimento_execucao$controller.txt &
+
+				sleep 10
+
+				echo "Estartando o mininet wifi e criando uma topologia simples...."
+				topology 127.0.0.1 6653 Floodlight $quantity &
+
+				while [ $(ps -ef | grep "jar target/floodlight.jar" | wc -l) -eq 2 ];
+				do
+					# echo  $(ps -ef | grep "target/floodlight.jar" | wc -l)
+					echo "Experimento do controlador $controller ainda está sendo realizado..."
+					sleep 2
+				done
+
+				echo "Experimento do controlador $controller finalizado!"
 			done
-		
-			echo "Experimento do controlador $controller finalizado!"
-			
-    	;;      
+
+    	;;
 
 		"POX")
 			#echo "sim " $controller;
-    	;;    
-  
-		"Maestro")		
+    	;;
+
+		"Maestro")
 			echo "sim " $controller;
     	;;
 
 		"Trema")
 			echo "sim " $controller;
-    	;;    
-  
-		"Beacon")		
+    	;;
+
+		"Beacon")
 			echo "sim " $controller;
-    	;;      
+    	;;
     esac
 done
--- Experimento/mininet_wifi.sh
+++ Experimento/mininet_wifi.sh
@@ -2,9 +2,10 @@
 
 function topology(){
 	# $1 - ip
-	# $2 - port	
+	# $2 - port
+	# $3 - nameController
+	# $4 - quantityNodes
 	cd /home/mininet-wifi/examples
-	sudo python iot4.py $1 $2
-	
-}
+	sudo python iot4.py $1 $2 $3 $4
 
+}
