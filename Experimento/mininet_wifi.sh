97bac87ea32f2129725f3d5b71f9903130447de6
RandelSouza <30515957+RandelSouza@users.noreply.github.com>
2019-08-24 21:07:31 -0300

    Update mininet_wifi.sh

--- Experimento/mininet_wifi.sh
+++ Experimento/mininet_wifi.sh
@@ -10,8 +10,6 @@ function topology(){
 
 	/usr/bin/time -f "Tempo decorrido: %E\nPercentual da CPU: %P\nTempo real decorrido relacionado ao processo: %e" \
 	sudo python /home/randel/mininet-wifi/examples/cenariosIoT.py $1 $2 $3 $4 $5 2>>  errorCenariosIoT$3_$4.txt > /dev/null 2>> ~/TCC/Experimento/resultados_experimento/$6/time_execucao_$3_$4.txt
-	# $6 - loss (taxa de perda de pacotes)
-	# $7 - delay (atraso de trasmissão)
 	sudo python /home/randel/mininet-wifi/examples/cenariosIoT.py $1 $2 $3 $4 $5 $6 $7 2>>  errorCenariosIoT$3_$4_$6_$7.txt > /dev/null
 }
 
